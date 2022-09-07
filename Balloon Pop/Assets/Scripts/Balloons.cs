68b728edb9fd051a4c0507c47ce2811137f74a5e
Evan <evan.c405@stu.nebo.edu>
2022-09-07 14:20:04 -0600

    Made Balloon Project

---
+++ Balloon Pop/Assets/Scripts/Balloons.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloons : MonoBehaviour
+{
+    public int ScoreToGive = 1;
+    public int clickToPop = 3;
+    public float scaleToIncrease = 0.1f;
+    //public ScoreManager ScoreManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Reference ScoreManager Component
+        //scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+    }
+
+    void OnMouseDown()
+    {
+        //Reduce clicks by one
+        clickToPop --;
+
+        //Increase balloon size
+        transform.localScale += Vector3.one * scaleToIncrease;
+
+        if(clickToPop == 0)
+        {
+            //ScoreManager.IncreaseScoreText(scaleToGive);
+            Destroy(gameObject);
+        }
+    }
+}
